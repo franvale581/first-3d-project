a9a70360eb398e9ac432ab25070f18e5a840f604
fran <fran-valero@hotmail.com>
2024-03-14 02:37:03 -0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <>
+    <Spline scene="https://prod.spline.design/7LtAPZgVNEHL8EkI/scene.splinecode" />
+    <div className='button-container'>
+      <button className='button'>CV</button>
+      <button className='button'>LINKEDIN</button>
+      <button className='button'>GITHUB</button>
+    </div>
+    </>
+  );
+}
